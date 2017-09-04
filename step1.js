3dccc35b97ccc16c90e5cd6f6c810c231ecf5aac
Alexa Rockwell <a.rckwll@gmail.com>
2017-09-04 12:08:12 -0400

    hello world

---
+++ step1.js
@@ -0,0 +1,7 @@
+/* Installation or testing
+
+    Advised bring your own computer (BYOC)
+    Have your text editor and shell set up and ready to go //advice for freshman
+
+*/
+console.log("Hello World");
