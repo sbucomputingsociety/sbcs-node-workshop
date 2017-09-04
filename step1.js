e17ed8796ae2b679d8fe30f9637c13482b443c00
Alexa Rockwell <a.rckwll@gmail.com>
2017-09-04 12:12:38 -0400

    http connection

--- step1.js
+++ step1.js
@@ -3,5 +3,9 @@
     Advised bring your own computer (BYOC)
     Have your text editor and shell set up and ready to go //advice for freshman
 
+    step 1.1 : install and test "node"
+    console.log("hello world");
+
+    step 1.2 : make a file and run "node run.js" with console.log() inside of it
 */
 console.log("Hello World");
---
+++ step2_http.js
@@ -0,0 +1,10 @@
+/*
+    Run the following and navigate to : http://localhost:8080/
+*/
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
